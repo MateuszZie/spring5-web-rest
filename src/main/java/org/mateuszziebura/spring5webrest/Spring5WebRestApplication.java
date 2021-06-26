ceb69230feeaf303ad4438126c49e17fde6da427
Mateusz Ziebura <mateusz_860@op.pl>
2021-06-26 10:51:35 +0200

    Initializer

---
+++ src/main/java/org/mateuszziebura/spring5webrest/Spring5WebRestApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.spring5webrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebRestApplication.class, args);
+	}
+
+}
